fff89f6d320e0abe73e76ff03af5e1591387eed6
m-evdokimov <m.v.evdokimov1@gmail.com>
2020-09-15 13:58:00 +0500

    Create encrypt and decrypt fucntions in main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+def encrypt(text, s):
+    result = ''
+    abc = ' abcdefghijklmnopqrstuvwxyz'
+    for i in text:
+        result += abc[(abc.find(i) + s) % 27]
+    return result
+
+def decrypt(text, s):
+    return encrypt(text, -s)
+
 def main():
-    pass
+    print(encrypt('i am caesar', 3))
+    print(decrypt('lcdpcfdhvdu', 3))
 
 if __name__ == '__main__':
     main()
