e9fa70be4e784474820e34e1a1e3c61dc2f299a3
m-evdokimov <m.v.evdokimov1@gmail.com>
2020-09-15 16:19:54 +0500

    Replace encrypt and decrypt functions to single function cipher. Implement cipher function.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-def encrypt(text, s):
+def cipher(method=1):
+    text = input('Input string: ')
+    s = int(input('Input shift: '))
+    if method == -1:
+        s *= -1
     result = ''
     abc = ' abcdefghijklmnopqrstuvwxyz'
     for i in text:
         result += abc[(abc.find(i) + s) % 27]
-    return result
-
-def decrypt(text, s):
-    return encrypt(text, -s)
+    print(result)
 
 def main():
-    print(encrypt('i am caesar', 3))
-    print(decrypt('lcdpcfdhvdu', 3))
+    while 1:
+        ans = int(input('What you want to do? (1 - encrypt, -1 - decrypt, 0 - exit): '))
+        if ans == 1:
+            cipher()
+        elif ans == -1:
+            cipher(-1)
+        elif ans == 0:
+            break
+        else:
+            raise KeyError('Incorrect input')
+
 
 if __name__ == '__main__':
     main()
